8b210de521faa527d8a4549c9b44a8ccceef0556
janani <jananisk2000@gmail.com>
2025-07-01 01:22:43 +0800

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import vercel from '@astrojs/vercel/serverless';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     tailwind(),
     icon(),
